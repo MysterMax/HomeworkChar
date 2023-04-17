75b94ea5d364e0d8974c0373c048cae3feb47585
MysterMax <128633737+MysterMax@users.noreply.github.com>
2023-04-17 16:13:31 +0200

    Add files via upload
    homeworkChar

---
+++ HomeworkChar.java
@@ -0,0 +1,20 @@
+package Homeworkjava;
+
+public class HomeworkChar {
+    public static void main(String[] args) {
+        char q = 'b' ;
+        System.out.println(q-q);
+        int q1 = (int) q;
+
+
+        char w = 239;
+        System.out.println(w * 0);
+
+
+      char g = '!';
+        System.out.println(g % g);
+
+
+
+    }
+}
---
+++ IntegerToChar.java
@@ -0,0 +1,20 @@
+package Homeworkjava;
+
+public class IntegerToChar {
+    public static void main(String[] args) {
+        char q = 'b' ;
+        System.out.println((int) q);
+
+        char w = '2' ;
+        int w2 = Integer.parseInt(String.valueOf(w));
+        System.out.println(w2);
+
+        char g = '!' ;
+        int g3 = Character.getNumericValue(g);
+        System.out.println(g);
+
+
+
+
+    }
+}
